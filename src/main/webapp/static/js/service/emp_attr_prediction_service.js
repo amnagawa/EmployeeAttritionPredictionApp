2c22c05c00e87c008c2a2c9843d489f63a3d9d69
amnagawa <43350382+amnagawa@users.noreply.github.com>
2018-09-20 01:23:37 +0530

    Upadated emp_attr_prediction_service file

--- src/main/webapp/static/js/service/emp_attr_prediction_service.js
+++ src/main/webapp/static/js/service/emp_attr_prediction_service.js
@@ -4,7 +4,7 @@
 App.factory('EmployeeAttritionPrediction', ['$resource', function ($resource) {
 	//$resource() function returns an object of resource class
     return $resource(
-    		'http://localhost:8080/EmployeeAttritionPredictionApp/empAttrPrediction/:tradeId',
+    		'http://attritionpredictiondb.mysql.database.azure.com/empAttrPrediction/:tradeId',
     		//'http://sharetrade-env.us-east-2.elasticbeanstalk.com/tradeForecast/:tradeId',
     		{tradeId: '@tradeId'},//Handy for update & delete. id will be set with id of instance
     		{
